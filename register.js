aba40a27dea2e5af7244e8d7cf93505ebb2c15a8
Karl David <68324556+TheKarlDavid@users.noreply.github.com>
2020-09-13 10:44:07 +0800

    3rd commit

---
+++ register.js
@@ -0,0 +1,19 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDu8b5oa9CNk5sDIofIbFFSD3iYjaLBq6k",
+    authDomain: "chushi-83100.firebaseapp.com",
+    databaseURL: "https://chushi-83100.firebaseio.com",
+    projectId: "chushi-83100",
+    storageBucket: "chushi-83100.appspot.com",
+    messagingSenderId: "406994437332",
+    appId: "1:406994437332:web:992cb186eda09a2ab6e741"
+};
+// Initialize Firebase
+var defaultProject = firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore(); 
+
+
+function register(){
+    console.log("SUUBMIT")
+}
