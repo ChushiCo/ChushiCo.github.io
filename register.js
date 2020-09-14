4385bff5b639482b7c2d65877e03544f7e8dd4d3
Karl David <68324556+TheKarlDavid@users.noreply.github.com>
2020-09-14 12:59:31 +0800

    5th commit
    successful registration

--- register.js
+++ register.js
@@ -13,7 +13,93 @@ var defaultProject = firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore(); 
 
+var registerUser = document.getElementById('register-user-btn');
 
-function register(){
-    console.log("SUUBMIT")
-}
+
+    var register_user = document.getElementById('register-user');
+    
+register_user.addEventListener('click', function(e){
+    e.preventDefault();
+    if( ((register_user.registerFname.value != null) && (register_user.registerFname.value != "")) &&
+        ((register_user.registerLname.value != null) && (register_user.registerLname.value != "")) &&
+        ((register_user.registerMob.value != null) && (register_user.registerMob.value != "")) &&
+        ((register_user.registerEmail.value != null) && (register_user.registerEmail.value != "")) &&
+        ((register_user.registerPass.value != null) && (register_user.registerPass.value != "")) &&
+        ((register_user.registerStreet.value != null) && (register_user.registerStreet.value != "")) &&
+        ((register_user.registerBldg.value != null) && (register_user.registerBldg.value != "")) &&
+        ((register_user.registerCity.value != null) && (register_user.registerCity.value != "")) ){
+            
+            let today = new Date();
+            let dd = today.getDate();
+            let mm = today.getMonth()+1; 
+            let yyyy = today.getFullYear();
+            let month;
+            switch(mm){
+                case 1: month="January "; break;
+                case 2: month="February "; break;
+                case 3: month="March "; break;
+                case 4: month="April "; break;
+                case 5: month="May "; break;
+                case 6: month="June "; break;
+                case 7: month="July "; break;
+                case 8: month="August "; break;
+                case 9: month="September "; break;
+                case 10: month="October "; break;
+                case 11: month="November "; break;
+                case 12: month="December "; break;
+                    // default: break;
+            }
+
+            today= month + dd +", " +yyyy; 
+            console.log(today.toString())
+
+            // console.log($("#registerFname").val() +" " +$("#registerLname").val())
+            
+            db.collection("users").add({
+                first_name: register_user.registerFname.value,
+                last_name: register_user.registerLname.value,
+                mob_num: parseInt(register_user.registerMob.value),
+                email: register_user.registerEmail.value,
+                password: register_user.registerPass.value,
+                role: "Client",
+                date_start: today,
+                street: register_user.registerStreet.value,
+                bldg: register_user.registerBldg.value,
+                city: register_user.registerCity.value
+            }).then(function(doc) {
+                console.log("Document written with UID: ", doc.id);
+                window.location.href = "register-success.html";
+            })
+            .catch(function(error) {
+                console.error("Error adding document: ", error);
+            });
+        }
+        else{
+            console.log("ELSE")
+        }
+        
+});
+
+
+var email = "karl_david@dlsu.edu.ph";
+var password = "abcd1234";
+firebase
+  .auth()
+  .signInWithEmailAndPassword(email, password)
+  .then(function (user) {
+    console.log("user signed in");
+
+    var user = firebase.auth().currentUser;
+    if (user != null) {
+      console.log(user.email);
+    }
+  })
+  .catch(function (error) {
+    if (error.code == "auth/wrong-password") {
+      alert("wrong password");
+    } else {
+      alert(err.message);
+    }
+  });
+
+
